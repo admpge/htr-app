f6385246a7fa0020f9a43db12a7d01b55e27c74c
adampage <adampage1993@gmail.com>
2024-02-07 11:18:30 +0000

    Refactor code and remove unnecessary comments and whitespace

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
 const express = require('express');
 const multer = require('multer');
 const cors = require('cors');
-const { spawn } = require('child_process'); // Import the child_process module
+const { spawn } = require('child_process');
 
 const app = express();
 const port = 3000;
 
-app.use(cors()); // Enable CORS for all routes
+app.use(cors());
 
-// Middleware for handling multipart/form-data
 const upload = multer({
     dest: 'uploads/',
-    limits: { fileSize: 10 * 1024 * 1024 }, // for example, 10 MB
+    limits: { fileSize: 10 * 1024 * 1024 },
     fileFilter: (req, file, cb) => {
         if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png') {
             cb(null, true);
@@ -20,8 +19,6 @@ const upload = multer({
         }
     },
 });
-
-app.use(express.static('public')); // Serve static files from the public directory
 
 app.post('/upload', upload.single('file'), (req, res) => {
     const uploadedFilePath = req.file.path;
@@ -32,22 +29,18 @@ app.post('/upload', upload.single('file'), (req, res) => {
     let result = '';
 
     pythonProcess.stdout.on('data', data => {
-        // Append data to result instead of sending a response immediately
         result += data.toString();
     });
 
     pythonProcess.stderr.on('data', data => {
-        // Log errors but don't send a response here
         console.error('Error from Python script:', data.toString());
     });
 
     pythonProcess.on('close', code => {
         console.log(`Python script finished with code ${code}`);
         if (code === 0) {
-            // Now that the script is done, send the accumulated result as JSON
             res.json({ text: result });
         } else {
-            // Only send an error response if the script fails
             res.status(500).send('Error processing the image');
         }
     });
