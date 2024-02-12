3fd5c2db9676bac80e29bf32bd00ecf2629c8c83
adampage <adampage1993@gmail.com>
2024-02-12 14:02:01 +0000

    Update header title in Header component

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,8 +1,7 @@
 const Header = () => (
-    <header className="bg-gradient-to-r from-blue-500 to-green-500 py-4">
+    <header className="bg-gradient-to-r from-blue-800 to-green-500 py-4">
         <div className="container mx-auto flex justify-between items-center">
-            <h1 className="text-white text-xl font-semibold">Handwritten Note Digitizer</h1>
-            {/* If you have navigation or other content to add to the header, it can go here */}
+            <h1 className="text-white text-xl font-semibold">Handwritten Note Digitiser</h1>
         </div>
     </header>
 );
